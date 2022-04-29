42c31fa06e027934d67a10d3381c70f36028f446
gihun3645 <gihun3645@naver.com>
2022-04-29 10:30:45 +0900

    Update 10430.py
    BOJ 10430.py

--- 10430.py
+++ 10430.py
@@ -1,4 +1,11 @@
-print((A+B)%C)
-print(((A%C) + (B%C))%C)
-print((A*B)%C)
-print(((A%C)*(B%C))%C)
+A, B, C = map(int, input().split())
+# 3개의 수를 입력받는다. 
+
+# map 함수를 쓰지 않는 경우
+# A, B ,C = input().split()
+# A = int(A)
+# B = int(B)
+# C = int(C)
+
+print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
+# sep='\n'로 줄바꿈
