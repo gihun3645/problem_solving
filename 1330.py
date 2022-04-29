c2ae7df676083bc260d469f96ad204c35dd54914
gihun3645 <gihun3645@naver.com>
2022-04-29 17:20:54 +0900

    BOJ 1330 두 수 비교하기

--- 1330.py
+++ 1330.py
@@ -1,11 +1,8 @@
-a = int(input())
-if 90<= a <=100:
-    print('A')
-elif 80<= a <= 89:
-    print('B')
-elif 70<= a <= 79:
-    print('C')
-elif 60<= a <= 69:
-    print('D')
+A,B = map(int,input().split())
+
+if A > B:
+    print('>')
+elif A < B:
+    print('<')
 else:
-    print('F')
+    print('==')
