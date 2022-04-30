d0674b129cdcbe370931f815e7c3f0959304a4a3
gihun3645 <gihun3645@naver.com>
2022-04-30 15:13:43 +0900

    BOJ
    BOJ 2884.py

--- 2884.py
+++ 2884.py
@@ -1,7 +1,10 @@
 H, M = map(int, input().split())
 # 두 개의 정수를 받음
 
-if M >= 45: 
-    print(H, M - 45)
-else:
-    print(H - 1, 60 - (45-M))
+if M < 45 :
+    if H == 0 : # 0 시이면 
+        H == 23
+        M += 60
+    else: # 0시가 아니면 (0시 보다 크면)
+        H -= 1
+        M += 60
