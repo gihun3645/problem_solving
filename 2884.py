c7670329a03e5678aa84392f149d1eba401e8c0f
gihun3645 <gihun3645@naver.com>
2022-04-30 15:16:10 +0900

    Update 2884.py
    수정

--- 2884.py
+++ 2884.py
@@ -3,8 +3,9 @@ H, M = map(int, input().split())
 
 if M < 45 :
     if H == 0 : # 0 시이면 
-        H == 23
+        H = 23
         M += 60
     else: # 0시가 아니면 (0시 보다 크면)
         H -= 1
         M += 60
+print(H, M-45)
