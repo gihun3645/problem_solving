3496068428569d458f6295f1d73ae4fb22b6ffdc
gihun3645 <gihun3645@naver.com>
2022-06-05 02:37:08 +0900

    Update 10809.py

--- 10809.py
+++ 10809.py
@@ -1,4 +1,5 @@
-string = input()
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in alphabet:
-    print(string.find(i), end=' ')
+word = input()
+alphabet = list(range(97, 123))  # 아스키코드 숫자 범위
+
+for x in alphabet:
+    print(word.find(chr(x)))
