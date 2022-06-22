35fa2ad8c2fdc88016a6e382fc60cd7146326f45
gihun3645 <gihun3645@naver.com>
2022-06-22 12:12:56 +0900

    코드업 1035

---
+++ PS/src/CodeUp/q1035.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.*;
+
+public class q1035 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int b = Integer.parseInt(a, 16);
+        System.out.println(Integer.toOctalString((b)));
+    }
+}
