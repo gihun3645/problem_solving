8786ff9626a18aaeba342456d71d9514bcd8b6c6
gihun3645 <gihun3645@naver.com>
2022-07-02 14:52:57 +0000

    BOJ 2914.java

---
+++ PS/src/BOJ/q2914.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+// https://st-lab.tistory.com/24
+
+import java.util.*;
+
+public class q2914 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*(sc.nextInt()-1)+1);
+    }
+}
