286e54fe5cefe6cf56cb75e83167be57d1786145
gihun3645 <gihun3645@naver.com>
2022-07-02 18:10:25 +0000

    CodeUp 1071.java(누락된 부분)

--- PS/src/CodeUp/q1071.java
+++ PS/src/CodeUp/q1071.java
@@ -7,7 +7,14 @@ public class q1071 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
+
+        while(true) {
+            int n = sc.nextInt();
+
+            if(n==0)
+                break;
+            System.out.println(n);
+        }
     }
     
 }
