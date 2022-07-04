33094715367f9dd45a6574394e0dd7175b554401
gihun3645 <gihun3645@naver.com>
2022-07-04 10:34:20 +0000

    CodeUp 1084 시간초과 해결 Java

--- PS/src/CodeUp/q1084.java
+++ PS/src/CodeUp/q1084.java
@@ -1,25 +1,46 @@
 package CodeUp;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class q1084 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int cnt = 0;
+        // 시간 초과
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int cnt = 0;
 
-        for(int i=0; i<a; i++) {
-            for(int j=0; j<b; j++) {
-                for(int k=0; k<c; k++) {
-                    System.out.println(i+" "+j+" "+k);
-                    cnt+=1;
+        // for(int i=0; i<a; i++) {
+        //     for(int j=0; j<b; j++) {
+        //         for(int k=0; k<c; k++) {
+        //             System.out.println(i+" "+j+" "+k);
+        //             cnt+=1;
+        //         }
+        //     }
+        // }
+        // System.out.println(cnt);
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] a = br.readLine().split(" ");
+
+        int cnt = 0 ;
+        for(int i=0; i<Integer.valueOf(a[0]); i++) {
+            for(int j=0; j<Integer.valueOf(a[1]); j++) {
+                for(int k=0; k<Integer.valueOf(a[2]); k++) {
+                    bw.write(i+" "+j+" "+k+"\n");
+                    cnt++;
                 }
             }
         }
-        System.out.println(cnt);
+        bw.write(String.valueOf(cnt));
+        bw.flush();
     }
 }
