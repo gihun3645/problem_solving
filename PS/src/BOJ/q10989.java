7cdf986f15fb8139bef1ec890430c283c31d38ef
gihun3645 <gihun3645@naver.com>
2022-07-12 08:35:11 +0900

    Create q10989.java

---
+++ PS/src/BOJ/q10989.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.*;
+
+public class q10989 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
