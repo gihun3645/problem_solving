b997fa9f972960921147700cfa03ead47f5de5e3
gihun3645 <gihun3645@naver.com>
2022-07-11 16:51:19 +0900

    백업
    백업

---
+++ PS/src/CodeUp/q1099.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.*;
+
+public class q1099 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
