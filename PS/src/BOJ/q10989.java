8d0e8b4b60013ef51ef735718c1290670cea051f
gihun3645 <gihun3645@naver.com>
2022-07-14 00:23:40 +0900

    BOJ 10989 Java(블로그 배낌)

--- PS/src/BOJ/q10989.java
+++ PS/src/BOJ/q10989.java
@@ -1,12 +1,30 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class q10989 {
 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
