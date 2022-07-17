20475e3cb8d32d66fa86f5b8bd3d5c6558f35d3b
gihun3645 <gihun3645@naver.com>
2022-07-17 16:44:32 +0900

    BOJ 3046 Java

---
+++ PS/src/BOJ/q3046.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.*;
+
+public class q3046 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(2*m - n);
+    }
+}
